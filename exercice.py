6795d9ae7a09566f689344d4f248c62492344e2e
LinaKhial <lina.khial@polymtl.ca>
2020-10-05 15:35:27 -0400

    LK: smth wrong with 4th function

--- exercice.py
+++ exercice.py
@@ -9,50 +9,78 @@ def reverse_data(data: list = None):
     # TODO: Demander 10 valeurs à l'utilisateur,
     # les stocker dans une structure de données,
     # et les retourner en ordre inverse, sans utiliser de liste.
+    stack , MAX = Stack() , 10
 
     if data is None:
-        pass  # Demander les valeurs ici
-
-    reversed_data = None  # Stocker le résultat ici
+        stack.put_many(input(f"Entrez la valuer {i} ici -> ") for i in range(MAX))
+    
+    reversed_data = [ stack.get() for _ in range(MAX) ]
 
     return reversed_data
 
 
 def delete_nth_from_stack(data: Stack, position: int) -> Stack:
     # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
-    return Stack()
+    for i in range(len(data)) :
+        if i == (len(data) - position) :
+            print(f"L'element a supprimer est {data.get()} a la position {position}.")
+            data.get()
+        else :
+            data.put(data.get())
+
+    new_stack = Stack()
+    new_stack.put_many(data.get() for _ in range(len(data)))
+   
+    return new_stack
 
 
 def delete_nth_from_queue(data: Queue, position: int) -> Queue:
     # TODO: Supprimer le énième (position) élément de data et retourner la nouvelle structure de données.
-    return Queue()
+    # something weird with this function
+    for i in range(len(data)) :
+        if i == position :
+            print(f"L'element a supprimer est {data.get()} a la position {position}.")
+            data.get()
+        else :
+            data.put(data.get())
+    return data
 
 
 def sort_stack(data: Stack) -> Stack:
     # TODO: Retourner la séquence triée
-    return Stack()
+    data_list = [data.get() for _ in range(len(data))]
+    data.put_many(sorted(data_list))
+    return data
 
 
 def sort_queue(data: Queue) -> Queue:
     # TODO: Retourner la séquence triée
-    return Queue()
+    data_list = [data.get() for _ in range(len(data))]
+    data.put_many(sorted(data_list))
+    return data
 
 
 def string_and_structs(string: str) -> tuple:
     # TODO: Parcourez la chaîne de caractères.
     # Si le caractère est une lettre, on l'ajoute dans fifo.
     # Sinon, on retire un élément de fifo pour l'insérer dans lifo.
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
     fifo, lifo = Queue(), Stack()
+    for letter in string :
+        if letter in alphabet :
+            fifo.put(letter)
+        else :
+            lifo.put(fifo.get())
 
     return fifo, lifo
 
 
 def main() -> None:
-    print("On inverse des données...")
-    print(f"Résultat: {reverse_data()}")
+    #print("On inverse des données...")
+    #print(f"Résultat: {reverse_data()}")
 
-    n = 4
+    '''n = 4
     lifo = Stack()
     lifo.put_many([i for i in range(20)])
     print(f"On retire l'élément à la position {n} de la pile et on obtient: {delete_nth_from_stack(lifo, n)}")
@@ -60,15 +88,16 @@ def main() -> None:
     n = 6
     fifo = Queue()
     fifo.put_many([i for i in range(20)])
+    print(f"Avant -> {fifo}")
     print(f"On retire l'élément à la position {n} de la file et on obtient: {delete_nth_from_queue(fifo, n)}")
 
     lifo = Stack()
     lifo.put_many([randint(0, 1000) for _ in range(20)])
-    print(f"On ordonne une file: {sort_queue(lifo)}")
+    print(f"On ordonne une pile: {sort_queue(lifo)}")
 
     fifo = Queue()
     fifo.put_many([randint(0, 1000) for _ in range(20)])
-    print(f"On ordonne une file: {sort_queue(fifo)}")
+    print(f"On ordonne une file: {sort_stack(fifo)}")'''
 
     sequence = "te!eYy.E6e/T"
     print(f"Le résulat de la manipulation de la séquence: {string_and_structs(sequence)}")
